9091b42e5c89a569de62f8acd46bf4d8491d2e56
muhammed-sawad-egov <muhammed.sawad@egov.global>
2026-09-16 12:41:06 +0530

    Set Tailwind dark mode to class strategy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
